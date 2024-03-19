84ab17d1dc08aecb0d398ee2adab8b70db9cb2d4
Robin-Ananou <robin2.ra8@gmail.com>
2024-03-19 15:52:28 +0100

    Commit Initial

---
+++ app/src/main/java/fr/somataru/all4sport/ScanQrCodeActivity.java
@@ -0,0 +1,99 @@
+package fr.somataru.all4sport;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanQrCodeActivity extends AppCompatActivity {
+
+    public static final String QR_CODE_KEY = "qr_code_key";
+    private static final int CAMERA_REQUEST_CODE = 23;
+
+    private SurfaceView scanSurfaceView;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr_code);
+
+        scanSurfaceView = findViewById(R.id.scan_surface_view);
+        initBarcodeDetector();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (cameraPermissionGranted(requestCode, grantResults)) {
+            finish();
+            overridePendingTransition(0, 0);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+        }
+    }
+
+    private boolean cameraPermissionGranted(int requestCode, int[] grantResults) {
+        return true;
+    }
+
+
+    private void initBarcodeDetector() {
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        initCameraSource();
+        initScanSurfaceView();
+    }
+
+    private void initCameraSource() {
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true)
+                .build();
+    }
+
+    private void initScanSurfaceView() {
+        scanSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(ScanQrCodeActivity.this, Manifest.permission.CAMERA) ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        cameraSource.start(holder);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(ScanQrCodeActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                // Nothing to do here
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.release();
+            }
+        });
+    }
+
+}
